337ea7c7278fea33e7eadea5ee0f95fb57cffd52
xcodereleases <xcodereleases@icloud.com>
2018-07-03 11:43:02 -0600

    Xcode 10 beta 3

--- xcodereleases/Data/Xcode10.swift
+++ xcodereleases/Data/Xcode10.swift
@@ -10,6 +10,14 @@ import Foundation
 
 let xcodes10: Array<Xcode> = [
     
+    Xcode(version: V("10L201y", "10.0", .beta(3)),
+          date: (2018, 07, 03),
+          requires: "10.13.4",
+          sdks: SDKs(macOS: V("18A326e", "10.14"), iOS: V("16A5318d", "12.0"), watchOS: V("16R5313d", "5.0"), tvOS: V("16J5313d", "12.0")),
+          compilers: Compilers(clang: V("1000.10.38", "10.0.0"), swift: V("1000.0.25.1", "4.2")),
+          links: Links(download: Link("https://download.developer.apple.com/Developer_Tools/Xcode_10_beta_3/Xcode_10_beta_3.dmg"),
+                       notes: Link("https://download.developer.apple.com/Developer_Tools/Xcode_10_Beta_3/Xcode_10_Beta_3_Release_Notes.pdf"))),
+    
     Xcode(version: V("10L177m", "10.0", .beta(2)),
           date: (2018, 06, 19),
           requires: "10.13.4",
