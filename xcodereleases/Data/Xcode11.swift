598b47e20c2c1b04c96e043136843f18748bf61b
xcodereleases <38095065+xcodereleases@users.noreply.github.com>
2019-06-17 11:37:49 -0600

    Xcode 11 beta 2

--- xcodereleases/Data/Xcode11.swift
+++ xcodereleases/Data/Xcode11.swift
@@ -10,7 +10,15 @@ import Foundation
 
 let xcodes11: Array<Xcode> = [
     
-    Xcode(version: V("11M336w", "11.0.0", .beta(1)),
+    Xcode(version: V("11M337n", "11.0", .beta(2)),
+          date: (2019, 06, 17),
+          requires: "10.14.4",
+          sdks: SDKs(macOS: V("19A487k", "10.15"), iOS: V("17A5508l", "13.0"), watchOS: V("17R5507k", "6.0"), tvOS: V("17J5501l", "13.0")),
+          compilers: Compilers(clang: V("1100.0.20.17", "11.0.0"), swift: V("1100.0.43.3", "5.1")),
+          links: Links(download: Link("https://download.developer.apple.com/Developer_Tools/Xcode_11_Beta_2/Xcode_11_Beta_2.xip"),
+                       notes: Link("https://developer.apple.com/documentation/xcode_release_notes/xcode_11_beta_2_release_notes"))),
+    
+    Xcode(version: V("11M336w", "11.0", .beta(1)),
           date: (2019, 06, 03),
           requires: "10.14.4",
           sdks: SDKs(macOS: V("19A471t", "10.15"), iOS: V("17A5492t", "13.0"), watchOS: V("17R5491t", "6.0"), tvOS: V("17J5485u", "13.0")),
