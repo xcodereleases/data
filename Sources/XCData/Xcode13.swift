2226657f0454ccc0a7188d9ed8fa340695c69290
xcodereleases <xcodereleases@icloud.com>
2021-10-26 17:43:50 -0600

    Latest information
    Xcode 13.0 b4, b5, rc1, release
    Xcode 13.1 rc1, release

--- Sources/XCData/Xcode13.swift
+++ Sources/XCData/Xcode13.swift
@@ -17,9 +17,63 @@ Xcode(version: V("<#build number#>", "<#version number#>", .<#release type#>),
       links: Links(download: Link("<#direct download link#>"),
                    notes: Link("<#direct release notes link#>")),
       checksums: Checksums(sha1: "<#shasum checksum#>")),
+ 
  */
 
 let xcodes13: Array<Xcode> = [
+    Xcode(version: V("13A1030d", "13.1", .release),
+          date: (2021, 10, 25),
+          requires: "11.3",
+          sdks: SDKs(macOS: V("21A344", "12.0"), iOS: V("19A339", "15.0"), watchOS: V("19R351", "8.0.1"), tvOS: V("19J344", "15.0")),
+          compilers: Compilers(clang: V("1300.0.29.3", "13.0.0"), swift: V("1300.0.31.4", "5.5.1")),
+          links: Links(download: Link("https://download.developer.apple.com/Developer_Tools/Xcode_13.1/Xcode_13.1.xip"),
+                       notes: Link("https://developer.apple.com/documentation/xcode-release-notes/xcode-13_1-release-notes")),
+          checksums: Checksums(sha1: "b55777d005c7774e4756f29d875d241db1769d90")),
+    
+    Xcode(version: V("13A1030d", "13.1", .rc(1)),
+          date: (2021, 10, 18),
+          requires: "11.3",
+          sdks: SDKs(macOS: V("21A344", "12.0"), iOS: V("19A339", "15.0"), watchOS: V("19R351", "8.0.1"), tvOS: V("19J344", "15.0")),
+          compilers: Compilers(clang: V("1300.0.29.3", "13.0.0"), swift: V("1300.0.31.4", "5.5.1")),
+          links: Links(download: Link("https://download.developer.apple.com/Developer_Tools/Xcode_13.1_Release_Candidate/Xcode_13.1_Release_Candidate.xip"),
+                       notes: Link("https://developer.apple.com/documentation/xcode-release-notes/xcode-13_1-release-notes")),
+          checksums: Checksums(sha1: "b55777d005c7774e4756f29d875d241db1769d90")),
+    
+    Xcode(version: V("13A233", "13.0", .release),
+          date: (2021, 09, 20),
+          requires: "11.3",
+          sdks: SDKs(macOS: V("20E214", "11.3"), iOS: V("19A339", "15.0"), watchOS: V("19R341", "8.0"), tvOS: V("19J344", "15.0")),
+          compilers: Compilers(clang: V("1300.0.29.3", "13.0.0"), swift: V("1300.0.31.1", "5.5")),
+          links: Links(download: Link("https://download.developer.apple.com/Developer_Tools/Xcode_13/Xcode_13.xip"),
+                       notes: Link("https://developer.apple.com/documentation/xcode-release-notes/xcode-13-release-notes")),
+          checksums: Checksums(sha1: "424ea14d86899290722906b3790af37ce9eea606")),
+    
+    Xcode(version: V("13A233", "13.0", .rc(1)),
+          date: (2021, 09, 14),
+          requires: "11.3",
+          sdks: SDKs(macOS: V("20E214", "11.3"), iOS: V("19A339", "15.0"), watchOS: V("19R341", "8.0"), tvOS: V("19J344", "15.0")),
+          compilers: Compilers(clang: V("1300.0.29.3", "13.0.0"), swift: V("1300.0.31.1", "5.5")),
+          links: Links(download: Link("https://download.developer.apple.com/Developer_Tools/Xcode_13_Release_Candidate/Xcode_13_Release_Candidate.xip"),
+                       notes: Link("https://developer.apple.com/documentation/xcode-release-notes/xcode-13-release-notes")),
+          checksums: Checksums(sha1: "424ea14d86899290722906b3790af37ce9eea606")),
+    
+    Xcode(version: V("13A5212g", "13.0", .beta(5)),
+          date: (2021, 08, 10),
+          requires: "11.3",
+          sdks: SDKs(macOS: V("21A5304f", "12.0"), iOS: V("19A5318g", "15.0"), watchOS: V("19R5323f", "8.0"), tvOS: V("19J5325f", "15.0")),
+          compilers: Compilers(clang: V("1300.0.29.3", "13.0.0"), swift: V("1300.0.29.102", "5.5")),
+          links: Links(download: Link("https://download.developer.apple.com/Developer_Tools/Xcode_13_beta_5/Xcode_13_beta_5.xip"),
+                       notes: Link("https://developer.apple.com/documentation/xcode-release-notes/xcode-13-beta-release-notes")),
+          checksums: Checksums(sha1: "1d01ddcfa634c5ef2d858c1b3284e42cd9c928b7")),
+    
+    Xcode(version: V("13A5201i", "13.0", .beta(4)),
+              date: (2021, 07, 27),
+              requires: "11.3",
+              sdks: SDKs(macOS: V("21A5294d", "12.0"), iOS: V("19A5307d", "15.0"), watchOS: V("19R5312d", "8.0"), tvOS: V("19J5314d", "15.0")),
+              compilers: Compilers(clang: V("1300.0.27.3", "13.0.0"), swift: V("1300.0.27.6", "5.5")),
+              links: Links(download: Link("https://download.developer.apple.com/Developer_Tools/Xcode_13_beta_4/Xcode_13_beta_4.xip"),
+                           notes: Link("https://developer.apple.com/documentation/xcode-release-notes/xcode-13-beta-release-notes")),
+              checksums: Checksums(sha1: "0c2c2b0d0a9853390bf7accb00cc95c3723277a3")),
     
     /*
      Beta 3 was re-released two days after its initial release.
