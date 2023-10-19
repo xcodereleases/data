efe84802ef3120940bc8dd1b3ee6dca2b28a9844
xcodereleases <xcodereleases@icloud.com>
2023-10-19 12:17:37 -0600

    Rework "isCurrent" determination

--- Sources/XCData/Xcodes.swift
+++ Sources/XCData/Xcodes.swift
@@ -37,6 +37,10 @@ extension Xcode {
         return OperatingSystemVersion(majorVersion: major, minorVersion: minor, patchVersion: patch)
     }
     
+    private var majorVersion: Int {
+        self.osVersion.majorVersion
+    }
+    
     private var sortingNumber: Int {
         let v = osVersion
         
@@ -56,53 +60,75 @@ extension Xcode {
         return number
     }
     
+    private var groupingNumber: Int {
+        let v = osVersion
+        
+        var number = v.majorVersion * 100_000
+        number += v.minorVersion * 10_000
+        number += v.patchVersion * 1_000
+        
+        return number
+    }
+    
     public static var allVersions: Array<Xcode> {
         var everything = xcodes15 + xcodes14 + xcodes13 + xcodes12 + xcodes11 + xcodes10 + xcodes9 + xcodes8 + xcodes7 + xcodes6 + xcodes5 + xcodes4 + xcodes3 + xcodes2 + xcodes1
         
+        // first, bucket everything by version
+        
         var groups = Dictionary<Int, Array<Xcode>>()
         for index in everything.indices {
+            // update in-place
             everything[index].isCurrent = false
             
             let xcode = everything[index]
-            let version = xcode.osVersion
-            let group = (version.majorVersion * 100) + version.minorVersion
+            let group = xcode.groupingNumber
             groups[group, default: []].append(xcode)
         }
         
-        var currentRelease: Xcode?
-        var currentBeta: Xcode?
-        
-        for group in groups.keys.sorted(by: >) {
-            let sortedXcodes = groups[group]!.sorted(by: { l, r in
-                return l.sortingNumber > r.sortingNumber
-            })
+        var latestRelease: Xcode?
+        var currentXcodes = Array<Xcode>()
+        for group in groups.keys.sorted(by: <) {
+            let sortedXcodes = groups[group]!.sorted(by: { $0.sortingNumber < $1.sortingNumber })
+            guard let lastXcodeInGroup = sortedXcodes.last else { continue }
             
-            let first = sortedXcodes.first!
-            if currentRelease == nil && first.releaseKind.isReleased {
-                currentRelease = first
-            } else if currentBeta == nil && first.releaseKind.isBeta {
-                currentBeta = first
+            if lastXcodeInGroup.releaseKind.isPrerelease {
+                currentXcodes.append(lastXcodeInGroup)
+            } else {
+                latestRelease = lastXcodeInGroup
             }
         }
         
-        if let beta = currentBeta, let release = currentRelease {
-            if release.osVersion.majorVersion == beta.osVersion.majorVersion {
-                currentBeta = nil
-            }
+        if let latestRelease {
+            currentXcodes.append(latestRelease)
         }
         
-        if let currentRelease, let idx = everything.firstIndex(of: currentRelease) {
-            print("\(currentRelease.displayName) is current")
-            everything[idx].isCurrent = true
-        }
-
-        if let currentBeta, let idx = everything.firstIndex(of: currentBeta) {
-            print("\(currentBeta.displayName) is current")
-            everything[idx].isCurrent = true
-        }
-
-        if everything.allSatisfy({ $0.isCurrent != true }) {
-            fatalError("Could not identify current release")
+        // there maybe be old xcodes for which release information is missing
+        // for example, we're missing data on the release of Xcode 12.1.1,
+        // so Xcode 12.1.1 RC is the last known version for that release.
+        // Since the RC is not a release version, the 12.1.1 RC is still
+        // being considered as "current", since nothing supercedes it.
+        //
+        // How do we reliably detect this, while still allowing for new
+        // prerelease versions of non-latest releases?
+        // ex: 15.2 is released, but there's a 15.1.2 RC with a critical bug fix.
+        //
+        // The policy this will take is that only the two most recent major versions
+        // will be considered "current".
+        // ex: 16.0 beta can be current while 15.x is current
+        //
+        // This would fail if a past Xcode (14.x) gets a critical update while
+        // 15.x and 16.x are current.
+        
+        let latestMajorVersion = currentXcodes.map(\.majorVersion).max()!
+        let rangeOfCurrentMajorVersions = (latestMajorVersion-1) ... latestMajorVersion
+        
+        currentXcodes = currentXcodes.filter { rangeOfCurrentMajorVersions.contains($0.majorVersion) }
+        
+        for releasedXcode in currentXcodes {
+            print("\(releasedXcode.displayName) is current")
+            if let idx = everything.firstIndex(of: releasedXcode) {
+                everything[idx].isCurrent = true
+            }
         }
         
         return everything
